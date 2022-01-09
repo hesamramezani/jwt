5a54f0cd9cbc44bab0b473b63c27a7d4e5d8a907
hesam <hesamramezani2121@gmail.com>
2022-01-09 10:03:03 -0500

    push it

---
+++ test1/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class contactmodel(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+
+
+
